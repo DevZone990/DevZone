185e76b26f3ce83860f634172f7c2ab2221ad4c4
benjaminGuirin <126121809+benjaminGuirin@users.noreply.github.com>
2023-07-03 19:01:17 -0300

    sc

--- js.js
+++ js.js
@@ -1,18 +1,13 @@
-// SECCION SCROLL ANIMACION
-const boxes = document.querySelectorAll('.hidden')
-window.addEventListener('scroll',checkBoxes)
- checkBoxes()
- function checkBoxes(){
-    const triggerBottom = window.innerHeight /5*4
-    boxes.forEach(box =>{
-        const boxTop = box.getBoundingClientRect().top
-        if (boxTop < triggerBottom){
-            box.classList.add('show')
-        }else{
-            box.classList.remove('show')
-        }
+const seccionesOcultas = document.querySelectorAll('.mob');
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        entry.target.classList.toggle('mostrar', entry.isIntersecting);
     })
- }
+})
+seccionesOcultas.forEach((seccion)=>observer.observe(seccion)); 
+
+
 
 
 //nav
